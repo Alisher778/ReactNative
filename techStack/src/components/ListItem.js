8c35e7a48c26bd6d3857c83324981d461f8f15e5
Alisher778 <alisher.musurmonov89@gmail.com>
2017-09-10 23:39:54 -0400

    finished Tech Stack App

--- techStack/src/components/ListItem.js
+++ techStack/src/components/ListItem.js
@@ -1,20 +1,36 @@
 import React from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Text, TouchableWithoutFeedback, View, LayoutAnimation } from 'react-native';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 class ListItem extends React.Component {
 	
+	componentWillUpdate() {
+		LayoutAnimation.spring();
+	}
+
+	renderDescription() {
+		const { id, description } = this.props.item;
+
+		if (id === this.props.selectedLibrary) {
+			return (
+				<Text style={styles.descriptionStyle}>{description}</Text>
+			);
+		}
+	}
+
+
 	render() {
-		
 		const { id, title } = this.props.item;
-		
 		return (
 			<TouchableWithoutFeedback onPress={() => this.props.selectLibrary(id)}>
 				<View>
 					<Text style={styles.flatList}>
 						{title}
 					</Text>
+					
+					{this.renderDescription()}
+					
 				</View>
 			</TouchableWithoutFeedback>
 		);
@@ -28,8 +44,18 @@ const styles = {
 		borderColor: '#ddd',
 		borderWidth: 0.5,
 		justifyContent: 'space-between',
-		fontSize: 20
+		fontSize: 20,
+		fontWeight: 'bold',
+		color: '#333'
+	},
+	descriptionStyle: {
+		padding: 10,
+		fontSize: 18
 	}
 };
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = (state) => {
+	return { selectedLibrary: state.selectedLibrary };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
