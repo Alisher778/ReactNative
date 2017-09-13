5a23717dda9042b4834c0a5644476e57e8d55b56
Alisher778 <alisher.musurmonov89@gmail.com>
2017-09-13 01:13:38 -0400

    added action creatores for password and email

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,27 +1,55 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
-import { Card, CardSection, Input } from './common';
+import { connect } from 'react-redux';
+import { emailChanged, passwordChanged } from '../actions';
+import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
+	onEmailChange(text) {
+		this.props.emailChanged(text);
+	}
+
+	onPasswordChange(text) {
+		this.props.passwordChanged(text);
+	}
+
 	render() {
+		console.log(this.props)
+		console.log(this.state)
 		return (
 			<Card>
 				<CardSection>
 					<Input 
 						label="Email"
-						placeholder="Your email"	
+						placeholder="Your email"
+						onChangeText={this.onEmailChange.bind(this)}
+						value={this.props.email}	
 					/>
 				</CardSection>
 				<CardSection>
 					<Input 
 						label="Password"
 						placeholder="Your password"
-						secureTextEntry	
+						secureTextEntry
+						onChangeText={this.onPasswordChange.bind(this)}
+						value={this.props.password}	
 					/>
+				</CardSection>
+				<CardSection>
+					<Button>
+						Login
+					</Button>
 				</CardSection>
 			</Card>
 		);
 	}
 }
 
-export default LoginForm;
+const mapStateToProps = state => {
+	return {
+		email: state.auth.email,
+		password: state.auth.password
+	};
+};
+
+export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
