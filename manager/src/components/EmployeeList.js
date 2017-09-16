8c584ef97dadc30d821475dcd04212a044dd40f4
Alisher778 <alisher.musurmonov89@gmail.com>
2017-09-16 01:17:19 -0400

    creating react app .

--- manager/src/components/EmployeeList.js
+++ manager/src/components/EmployeeList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Actions } from 'react-native-router-flux';
 import { connect } from 'react-redux';
 import _ from 'lodash';
-import { ListView, Text, View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 import { CardSection, Button } from './common';
 import { employeesFetch } from '../actions';
 
@@ -11,21 +11,17 @@ class EmployeeList extends Component {
 
 	componentWillMount() {
 		this.props.employeesFetch();
-		const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-	    this.state = {
-	      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
-	    };
 	}
 
 	render() {
-		console.log(this.props.employees)
 		return (
 			
 				<View>
-					<ListView
-        dataSource={this.state.dataSource}
-        renderRow={(rowData) => <Text>{rowData}</Text>}
-      />
+					<FlatList
+					  data={[{key: 'a'}, {key: 'b'}]}
+					  renderItem={({item}) => <Text>{item.key}</Text>}
+					/>
+
 					<CardSection>
 						<Button onPress={() => Actions.employeeCreate()}>
 							Add Employee
